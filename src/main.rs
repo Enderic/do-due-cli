2aff668fdc30028f48d0684f7bb344a560aa9bdd
Enderic <mahiyan21@gmail.com>
2025-05-30 10:41:53 -0500

    solidified the flow

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,43 @@ use task::Task;
 fn main() {
     let matches = command!()
         .subcommand(
-            Command::new("add")
+    Command::new("add")
                 .about("Add a new task")
+                /*
+                .arg(
+                    Arg::new("name")
+                        .index(1)
+                        .required(true)
+                        .help("The name of your task\nShould not be named the same as another task")
+                )
+                .arg(
+                    Arg::new("do-date")
+                        .index(2)
+                        .required(true)
+                        .help("When you should do this task")
+                )
+                .arg(
+                    Arg::new("due-date")
+                        .index(3)
+                        .required(true)
+                        .help("When the task is not doable anymore")
+                )
+                .arg(
+                    Arg::new("priority")
+                        .index(4)
+                        .required(false)
+                        .help("How important the task is to get done")
+                )
+                */
         )
         .get_matches();
 
     match matches.subcommand() {
-        Some(("add", _sub_m)) => {
+        Some(("add", _)) => {
             print!("Testing input: ");
             io::stdout().flush().unwrap();
             let mut input = String::new();
-            let _test = io::stdin().read_line(&mut input);
+            let test = io::stdin().read_line(&mut input);
             println!("You typed: {}", input);
         }
         _ => {
